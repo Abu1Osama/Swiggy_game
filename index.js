037db7aaec249e626d876fd038dc99966ee17d7a
Abu Osama <abuosamasports@gmail.com>
2023-11-19 15:53:54 +0530

    GameArena with main function playgame is created ,attack and defend dice checked

--- index.js
+++ index.js
@@ -17,8 +17,51 @@ let createPlayer = (health, strength, attack) => ({
     },
   });
 
-//   let player1 = createPlayer(100, 10, 5);
-// let player2 = createPlayer(100, 8, 6);
+  let GameArena = (playerA, playerB) => ({
+    players: [playerA, playerB],
+  
+  
+  // Main Game function
+  
+    playGame: () => {
+      let currentPlayerIndex = playerA.health < playerB.health ? 0 : 1;
+      let opponentIndex = 1 - currentPlayerIndex;
+  
+      while (playerA.health > 0 && playerB.health > 0) {
+        let attackDieRoll = Math.floor(Math.random() * 6) + 1;
+        let defendDieRoll = Math.floor(Math.random() * 6) + 1;
+        console.log(attackDieRoll);
+        console.log(defendDieRoll);
+  
+        let damageTaken = playerA.attackOpponent(
+          playerB,
+          attackDieRoll,
+          defendDieRoll
+        );
+  
+        console.log(
+          `Player ${currentPlayerIndex + 1} attacks Player ${
+            opponentIndex + 1
+          } with damage ${damageTaken}.`
+        );
+  
+        
+        currentPlayerIndex = 1 - currentPlayerIndex;
+        opponentIndex = 1 - currentPlayerIndex;
+      }
+  
+      // Print the winner
+      let winner = playerA.health > 0 ? 1 : 2;
+      console.log(`Player ${winner} wins the game!`);
+    },
+  });
 
-// let damage = player1.attackOpponent(player2, 0, 4);
-// console.log(`Player 2 took ${damage} damage. Player 2 health: ${player2.health}`);
+
+  let player1 = createPlayer(100, 10, 5);
+  let player2 = createPlayer(100, 8, 6);
+  const arena = GameArena(player1, player2);
+  arena.playGame();
+
+
+let damage = player1.attackOpponent(player2, 0, 4);
+console.log(`Player 2 took ${damage} damage. Player 2 health: ${player2.health}`);
